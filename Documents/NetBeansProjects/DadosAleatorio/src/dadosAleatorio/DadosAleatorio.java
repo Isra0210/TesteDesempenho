68a73122f7ab13f544d9c9c626e9a5ad8bb3d1a9
aspire <aspire@acer>
2019-09-06 14:15:10 -0300

    22/08/2019

--- Documents/NetBeansProjects/DadosAleatorio/src/dadosAleatorio/DadosAleatorio.java
+++ Documents/NetBeansProjects/DadosAleatorio/src/dadosAleatorio/DadosAleatorio.java
@@ -2,37 +2,43 @@ package dadosAleatorio;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.Timer;
 public class DadosAleatorio {
 
     public static void main(String[] args) throws UnknownHostException {
         int i;
-        
+
         long tempoInicial = System.currentTimeMillis();
-        
-        Timer timer1 = new Timer();
-        Timer timer2 = new Timer();
+
         Numeros n = new Numeros();
         Conexao conexao = new Conexao();
 
-//        int []vetor = new int[10000];
-        ArrayList<Integer> lista = new ArrayList<Integer>();
+        ArrayList<Integer> lista = new ArrayList<Integer>();;
         
         Random gerador = new Random();
         
         for (i = 1; i <= 10000; i++) {
             lista.add(gerador.nextInt());
-//            vetor[i] = gerador.nextInt();
         }
         
         for(int valores : lista){
             n.setNumero(valores);
             conexao.valoresAleatorios(n);
-            System.out.println(valores);
         } 
+        
         long tempoFinal = System.currentTimeMillis();
         
-        System.out.println("Tempo de execução: " + (tempoFinal - tempoInicial) + " Millisegundos");
+        long tempoMillisegundos = (tempoFinal - tempoInicial) ;
+        
+        System.out.println("Tempo de execução: " + tempoMillisegundos + " Millisegundos");
+        
+        long tempoSegundos = tempoMillisegundos / 1000;
+        
+        System.out.println("Tempo em segundos: " + tempoSegundos);
+        
+        long tempoMinutos = tempoSegundos / 60;
+        
+        System.out.println("Tempo em minutos: " + tempoMinutos);
+        
         System.exit(0);
     }
 }
